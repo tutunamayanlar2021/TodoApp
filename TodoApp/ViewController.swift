16cb390d1be4becaae8861afd671519b9f2f144b
tutunamayanlar2021 <kaderoral2147@gmail.com>
2023-08-01 11:33:49 +0300

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Kader Oral on 1.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
